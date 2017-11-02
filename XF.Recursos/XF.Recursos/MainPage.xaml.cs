27a018bf07f52bba37edf8f02ea4b60a6b232319
mespinasso <matheus.espinasso@gmail.com>
2017-11-01 22:19:33 -0200

    MessageQueue Demo

--- XF.Recursos/XF.Recursos/MainPage.xaml.cs
+++ XF.Recursos/XF.Recursos/MainPage.xaml.cs
@@ -38,5 +38,15 @@ namespace XF.Recursos
         {
             await Navigation.PushAsync(new Theme.ThemeView());
         }
+
+        private async void btnHome_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PassParameter.HomeView(DateTime.Now.ToString("u")));
+        }
+
+        private async void btnMC_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PassParameter.MCHomeView());
+        }
     }
 }
