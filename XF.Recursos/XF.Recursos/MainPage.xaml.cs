ba8b620f5df720c1413cb76bf3dd3df2ac9e6bab
mespinasso <matheus.espinasso@gmail.com>
2017-11-22 22:15:52 -0200

    Phone resources and general controls

--- XF.Recursos/XF.Recursos/MainPage.xaml.cs
+++ XF.Recursos/XF.Recursos/MainPage.xaml.cs
@@ -78,5 +78,35 @@ namespace XF.Recursos
         {
             await Navigation.PushAsync(new GPS.CoordenadaView());
         }
+
+        private async void btnLigar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new API.PhoneView());
+        }
+
+        private async void btnCamera_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new API.CameraView());
+        }
+
+        private async void btnData_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Controles.PickerView());
+        }
+
+        private async void btnProgresso_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Controles.ProgressoView());
+        }
+
+        private async void btnPicker_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Controles.ListPickerView());
+        }
+
+        private async void btnEditor_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Controles.EditorView());
+        }
     }
 }
